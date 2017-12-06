aa599374d8e534686bce67b0e5eef734be065d21
René Pfeuffer <rene@pfeuffer-web.de>
2017-12-06 18:20:00 +0100

    Add base path.

--- src/main/java/de/pfeufferweb/filewatch/ProcessController.java
+++ src/main/java/de/pfeufferweb/filewatch/ProcessController.java
@@ -5,8 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/motioncontrol/motion")
 public class ProcessController {
 
     private final ProcessRunner processRunner;
