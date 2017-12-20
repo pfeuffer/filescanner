06fcc3a31ea845b87504ef9173eac9f3b237321b
René Pfeuffer <rene@pfeuffer-web.de>
2017-12-20 15:04:08 +0100

    Remove spring security.
    This should be done by the infrastructure.

--- src/main/java/de/pfeufferweb/filewatch/MotionControlApplication.java
+++ src/main/java/de/pfeufferweb/filewatch/MotionControlApplication.java
@@ -6,9 +6,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -18,17 +15,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class MotionControlApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(MotionControlApplication.class, args);
-    }
-}
-
-@EnableWebSecurity
-@Configuration
-class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().fullyAuthenticated().and().
-                httpBasic().and().csrf().disable();
     }
 }
 
